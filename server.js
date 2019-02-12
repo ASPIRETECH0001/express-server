e285eb55d97eeaa8b1276efac20be75e26f1ba49
Maxym Borodenko <m.borodenko@aimprosoft.com>
2019-02-12 23:36:23 +0200

    Added `My projects` page

--- server.js
+++ server.js
@@ -49,6 +49,12 @@ app.get('/about', (req, res) => {
    });
 });
 
+app.get('/projects', (req, res) => {
+   res.render('projects.hbs', {
+       pageTitle: 'My projects page'
+   });
+});
+
 app.get('/bad', (req, res) => {
     res.status(500);
 
